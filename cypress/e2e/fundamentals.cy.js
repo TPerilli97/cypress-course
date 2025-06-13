c8d3259088ba5669d5cf2e90228aa081b145b64a
Tommaso Perilli <perillitommaso@yahoo.it>
2025-06-13 17:40:42 +0200

    New changes

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('Test - Access Fundamentals Page', () => {
     cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').should('exist')
+    cy.get('[data-test="fundamentals-header"]').should('contain', /Testing Fundamentals/i)
   })
 })
